206d0b9f9a4d9c2f189ad824316bd485b17a5305
胡镇 <huzhen555@qq.com>
2023-08-07 16:43:56 +0800

    fix: fix that match fail with empty string

--- src/matches/matchWithString.ts
+++ src/matches/matchWithString.ts
@@ -18,9 +18,9 @@ export default function matchWithString<T extends string | Record<any, any>>(
 ) {
 	const newStr = isFn(matchStr) ? matchStr(origin) : origin;
 	myAssert(isString(newStr), `${newStr} is not a string`);
-
 	const isOnMatchedFn = isFn(onMatched);
-	let position = [] as MatchedPosition[],
+	let isMatched = true,
+		position = [] as MatchedPosition[],
 		matchedRange: number[] = [],
 		lastIndex = -1,
 		indexes: number[] = [],
@@ -28,61 +28,66 @@ export default function matchWithString<T extends string | Record<any, any>>(
 		// 如果不需要转换匹配字符串则默认为被查找的字符串，否则会通过appendTransformedStr一步步追加内容上去
 		transformedStr = isOnMatchedFn ? '' : matchingStr;
 	const strArr = isOnMatchedFn ? [] : [matchingStr];
-	if (ignoreCase) {
-		matchingStr = matchingStr.toLowerCase();
-		matcher = matcher.toLowerCase();
-	}
 
-	let splitedStartIndex = 0;
-	const appendTransformedStr = () => {
-		// 替换匹配的字符串
-		if (isOnMatchedFn) {
-			const [startIndex, endIndex] = matchedRange,
-				notMatchSnippet = newStr.substring(splitedStartIndex, startIndex),
-				matchedSnippet = onMatched(newStr.substring(startIndex, endIndex + 1), origin);
-
-			// 截取初始字符串
-			transformedStr += notMatchSnippet;
-			transformedStr += matchedSnippet;
-			strArr.push(notMatchSnippet, matchedSnippet);
-			splitedStartIndex = endIndex + 1;
+	if (matcher) {
+		if (ignoreCase) {
+			matchingStr = matchingStr.toLowerCase();
+			matcher = matcher.toLowerCase();
 		}
-	};
+		let splitedStartIndex = 0;
+		const appendTransformedStr = () => {
+			// 替换匹配的字符串
+			if (isOnMatchedFn) {
+				const [startIndex, endIndex] = matchedRange,
+					notMatchSnippet = newStr.substring(splitedStartIndex, startIndex),
+					matchedSnippet = onMatched(newStr.substring(startIndex, endIndex + 1), origin);
 
-	// 对每个搜索字符单独查找
-	for (let i = 0; i < matcher.length; i++) {
-		const index = matchingStr.indexOf(matcher[i], lastIndex + 1); // 查找时需从上一个查找结果后开始
-		if (index >= 0) {
-			indexes.push(index);
-			if (matchedRange.length === 0) {
-				matchedRange = [index, index];
-			} else {
-				if (index === matchedRange[1] + 1) {
-					matchedRange[1] += 1;
-				} else {
-					appendTransformedStr();
-					position.push(convertIndex(matchedRange));
-					matchedRange = [index, index];
-				}
+				// 截取初始字符串
+				transformedStr += notMatchSnippet;
+				transformedStr += matchedSnippet;
+				strArr.push(notMatchSnippet, matchedSnippet);
+				splitedStartIndex = endIndex + 1;
 			}
-			lastIndex = index;
-		} else {
-			// 如果中途有某个字符没匹配，则表示不匹配此字符串，直接进行下一个字符串匹配，否则会导致顺序错乱
-			matchedRange = position = indexes = [];
-			break;
+		};
+
+		// 对每个搜索字符单独查找
+		for (let i = 0; i < matcher.length; i++) {
+			const index = matchingStr.indexOf(matcher[i], lastIndex + 1); // 查找时需从上一个查找结果后开始
+			if (index >= 0) {
+				indexes.push(index);
+				if (matchedRange.length === 0) {
+					matchedRange = [index, index];
+				} else {
+					if (index === matchedRange[1] + 1) {
+						matchedRange[1] += 1;
+					} else {
+						appendTransformedStr();
+						position.push(convertIndex(matchedRange));
+						matchedRange = [index, index];
+					}
+				}
+				lastIndex = index;
+			} else {
+				// 如果中途有某个字符没匹配，则表示不匹配此字符串，直接进行下一个字符串匹配，否则会导致顺序错乱
+				isMatched = false;
+				matchedRange = position = indexes = [];
+				break;
+			}
+		}
+		if (matchedRange.length > 0) {
+			appendTransformedStr();
+			if (isOnMatchedFn) {
+				// 将末尾的字符串补充上去
+				const endSnippet = newStr.substring(matchedRange[1] + 1);
+				transformedStr += endSnippet;
+				strArr.push(endSnippet);
+			}
+			position.push(convertIndex(matchedRange));
 		}
 	}
-	if (matchedRange.length > 0) {
-		appendTransformedStr();
-		if (isOnMatchedFn) {
-			// 将末尾的字符串补充上去
-			const endSnippet = newStr.substring(matchedRange[1] + 1);
-			transformedStr += endSnippet;
-			strArr.push(endSnippet);
-		}
-		position.push(convertIndex(matchedRange));
-	}
+
 	return {
+		isMatched,
 		position,
 		indexes,
 		str: transformedStr,
